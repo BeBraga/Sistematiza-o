b675f5ae7852ebcb564ff9c431d39235cf6e71ad
Bernardo Braga <Bernardo Braga@Bernardo>
2024-06-25 14:16:30 -0300

    Funcionario

--- Sistematização/src/sistematizacao/Funcionarios.java
+++
@@ -1,9 +0,0 @@
-package sistematizacao;
-import java.util.ArrayList;
-
-public class Funcionarios {
-	public static void main(String[] args) {
-		ArrayList<String> lista = new ArrayList<String>();
-	}
-
-}
